4fb00ab66ae426c1e950ba754b031b703dde2f15
Sahoo, Suman <suman.sahoo@fmr.com>
2021-02-16 17:38:18 +0530

    v

--- Homepage/urls.py
+++ Homepage/urls.py
@@ -19,4 +19,5 @@ from . import views
 
 urlpatterns = [
 path('', views.home, name='home'),
+path('services_page/', views.services_page, name='services_page'),
 ]
--- Homepage/views.py
+++ Homepage/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request,'index.html')
+
+
+def services_page(request):
+    return render(request,'index-1.html')
