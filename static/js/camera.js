b111c1e6783c427770acd01e6630c79572c30942
sumankalyan <sumankalyan123@gmail.com>
2021-02-17 13:24:58 +0530

    eee

--- static/js/camera.js
+++ static/js/camera.js
@@ -79,9 +79,9 @@
 		
 		thumbnails			: false,
 		
-		time				: 7000,	//milliseconds between the end of the sliding effect and the start of the nex one
+		time				: 3000,	//milliseconds between the end of the sliding effect and the start of the nex one
 		
-		transPeriod			: 1000,	//lenght of the sliding effect in milliseconds
+		transPeriod			: 700,	//lenght of the sliding effect in milliseconds
 		
 ////////callbacks
 
